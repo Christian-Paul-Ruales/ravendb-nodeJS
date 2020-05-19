d08d1884ed389b81ad89c93f13c3541c6677d467
Christian Ruales <christianpaulruales@gmail.com>
2020-05-18 23:28:44 -0500

    Ejemplo basico CRUD con RavenDB y NodeJS

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ store.initialize();
 const session = store.openSession();
 
 let element = {
-    title: "mi titulo",
+    title: "tITLE",
     numerico: 1,
     decimal: 99.99,
     fecha: new Date(),
@@ -14,44 +14,61 @@ let element = {
 }
 
 const running = async() => {
-  //  await create(element);
-    await read();
-    await update("Nuevo Titulo");
-    await read();
-    await del();
-    await read();
+    //  await queryAll()
+
+  const idCreated =  await create(element);
+  await read(idCreated);
+  await update(idCreated,"nuevo nuevo");
+  await read(idCreated);
+  await del(idCreated);
+  await read(idCreated);
+
+}
+
+const queryAll = async () => {
+    const query = await session.query({ collection: '@empty' }).whereEquals('title', 'Nuevo Titulo');
+    const resutls = await query.all();
+    console.log('-------------------------Ejemplo de query');
+    console.log(resutls);
 
 }
 
 const create = async( myElement) => {
+    console.log ("---------------------Crear")
+
     await session.store(myElement, 'element/');
-    console.log(myElement);
     await session.saveChanges();
+
+    return myElement.id;
 }
 
-const read = async() => {
-    let element = await session.load('element/0000000000000000001-A');
+const read = async(id) => {
     console.log ("---------------------Mostrar")
 
-    console.log (element)
+    let element = await session.load(id);
+    if(element !== null){
+        console.log (element)
+    }else {
+        console.log ("elemento "+id+ " NO ENCONTRADO")
+
+    }
+
 }
 
-const update = async(title) => {
+const update = async(id, title) => {
     console.log ("---------------------Actualizar")
 
-    let element= await session.load('element/0000000000000000001-A');
+    let element= await session.load(id);
     element.title = title;
     element.fecha = new Date();
     await session.saveChanges();
 
-    let elementUpdated= await session.load('element/0000000000000000001-A');
-
-
 }
 
-const del = async() => {
-    let element= await session.load('element/0000000000000000001-A');
-    await session.delete(product);
+const del = async(id) => {
+    console.log('-----------------Eliminar')
+    let element= await session.load(id);
+    await session.delete(element);
     await session.saveChanges();
    
 }
